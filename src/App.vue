d990fea77964320caf77a83817c4b53fc454bb4c
911harm <911harm@gmail.com>
2020-07-15 15:57:55 -0500

    commiting

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,466 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="nav col-sm pt-3">
+        <div class="logos mx-auto align-middle">
+          <a href="https://www.cpcagencia.com/#!/" target="_blanck">
+            <img class="logo" src="@/assets/img/cpc.png" alt="CPC Agencia">
+          </a>
+          <a href="https://www.mc.net.co/" target="_blanck">
+            <img class="logo" src="@/assets/img/Mc.png" alt="Media Commerce">
+          </a>
+          <div class="redes mx-auto">
+          <ul>
+            <li><a class="fa fa-facebook" href="https://www.facebook.com/CPCagencia/" target="_blanck"></a></li>
+            <li><a class="fa fa-instagram" href="https://www.instagram.com/cpcagencia/?hl=es" target="_blanck"></a></li>
+            <li><a class="fa fa-linkedin" href="https://www.linkedin.com/company/cpcgroup/" target="_blanck"></a></li>
+            <li><a class="fa fa-youtube" href="https://www.youtube.com/user/CPCagencia" target="_blanck"></a></li>
+          </ul>
+        </div> 
+        </div>
+    </div>
+    <div class="bkt mx-auto">
+      <h1 class="title-md font-weight-bold" >El mundo cerró,<br> internet esta abierto</h1>
+      <h2>Somos expertos en montar una tienda virtual</h2>
+       <button class="btn btn-test mx-auto btn-success rounded-pill btn-lg mb-5" data-toggle="modal" data-target="#Test">Hacer test</button>
+    </div>
+    <div class="mx-auto section2 mt-4"><!--container-->
+       <div class="container">
+          <h2 class="title-section2 display-3 font-weight-bold" >Nosotros</h2>
+          <h3>Tenemos 3 tipos de soluciones para montar tu tienda <br>virtual dependiendo de tu necesidad</h3>
+       </div>
+       <div class="row mx-auto mt-3" style="width:98%">
+         <div class="card col-lg-3 col-sm-12 mx-auto" style="width: 18rem;">
+         <div class="row">
+            <img class="img-card-tiendas d-block" src="@/assets/img/tienda-express.png" alt="TIENDA EXPRESS">
+            <div class="card-body col-8 text-left card-body-tiendas">
+              <h5 class="card-title font-weight-bold" style="font-size:2rem">TIENDA EXPRESS</h5>
+                <p class="card-text display-5">En tan solo 5 dias tendrás tu tienda con pasarela de pago para llevar por primera vez tus productos a digital.</p>
+                <button type="button" data-toggle="popover" data-container="body" data-placement="bottom" data-content="
+En tan solo 5 dias tendrás tu Tienda ONLINE con pasarela de pago para llevar por primera vez tus productos a digital. Por ello, con nuestras Tiendas Express, podrás tener rápidamente tus servicios en la Red y empezar a vender a todo el país de manera masiva, 24/7; a través de una amplia gama de plantillas personalizables con las que podrás crear tu Tienda como siempre has querido." class="btn btn-success btn-test btn-more">Ver más</button>
+            </div>
+         </div>
+        </div>
+         <div class="card col-lg-5 col-sm-12 mx-auto" style="width: 18rem;">
+          <div class="row">
+            <img class="img-card-tiendas d-block" src="@/assets/img/tienda-personalizada.png" alt="TIENDA PERSONALIZADA">
+            <div class="card-body col-8 text-left card-body-tiendas">
+              <h5 class="card-title font-weight-bold" style="font-size:2rem">TIENDA PERSONALIZADA</h5>
+                <p class="card-text display-5">Sabemos que tu producto necesita algunosdesarrollos a la medida, juntos construiremos tu solucion</p>
+                  <button type="button" data-toggle="popover" data-container="body" data-placement="bottom" data-content="
+Sabemos que tu producto necesita algunos desarrollos a la medida, juntos construiremos tu solución. Con nuestras Tiendas Personalizadas, podrás acceder a una amplia gama de opciones para tener tu tienda como siempre has querido, con presencia en el nuevo MarketPlace de MediaCommerce, a través del cual, podrás dar a conocer tus productos y servicios rápidamente. " class="btn btn-success btn-test btn-more">Ver más</button>
+            </div>
+          </div>
+        </div>
+         <div class="card col-lg-3 col-sm-12 mx-auto" style="width: 18rem;">
+          <div class="row">
+            <img class="img-card-tiendas d-block" src="@/assets/img/tienda-vtex.png" alt="TIENDA VTEX">
+            <div class="card-body col-8 text-left card-body-tiendas">
+              <h5 class="card-title font-weight-bold" style="font-size:2rem">TIENDA VTEX</h5>
+                <p class="card-text display-5">Quieres articular tu tieda con otras plataformas como inventario, facturación y despachos, estas buscando la mejor plataforma para gestionar toda tu estrategia e-commerce.</p>
+                  <button type="button" data-toggle="popover" data-container="body" data-placement="bottom" data-content="
+Si quieres articular tu tienda con otras plataformas como inventario, facturación y despachos, estás buscando la mejor plataforma para gestionar toda tu estrategia e-commerce. Para ello las Tiendas VTEX se articularán con todo tu equipo y te brindarán las mejores soluciones de personalización y estilo en tus ventas, además, tendrás los mejores beneficios de funcionalidad y seguridad en los procesos transaccionales de tus clientes. " class="btn btn-success btn-test btn-more">Ver más</button>
+            </div>
+          </div>
+        </div>
+       </div>
+       <button class="btn btn-test mx-auto btn-success rounded-pill btn-lg mb-5" data-toggle="modal" data-target="#Test">Hacer test</button>
+    </div>
+    <!--SECTION3-->
+    <div class="container my-5">
+      <div class="row">
+        <div class="col-sm-6 col-6 col-lg-3">
+          <h2 class="title-section2 text-center font-weight-bold" style="font-size:3rem">3254</h2>
+          <h5>ventas en el día sin IVA</h5>
+        </div>
+        <div class="col-sm-6 col-6 col-lg-3">
+          <h2 class="title-section2 text-center font-weight-bold" style="font-size:3rem">2.456.000</h2>
+          <h5>millones de ventas en un día</h5>
+        </div>
+        <div class="col-sm-6 col-6 col-lg-3">
+          <h2 class="title-section2 text-center font-weight-bold" style="font-size:3rem">90%</h2>
+          <h5>de los e-commerce en colombia son Vtex</h5>
+        </div>
+        <div class="col-sm-6 col-6 col-lg-3">
+          <h2 class="title-section2 text-center  font-weight-bold" style="font-size:3rem">76%</h2>
+          <h5>de probabilidad de volver a comprar quien ha realizado una compra por internet</h5>
+        </div>
+      </div>
+    </div>
+    <!--Section4-->
+    <div class="section4 ">
+      <h3 class="title-center pt-4 pb-2 title-blue-dark">MIRA NUESTRO VÍDEO</h3>
+      <div class="container fondo2">
+      </div>
+    </div>
+    <!--section5-->
+    <div class="container my-5">
+      <h2 class="title-center title-section2 display-3 font-weight-bold">CLIENTES FELICES</h2>
+      <h3 class="title-center">Lo que las personas dicen de nosotros <br>despues de utilizar nuestros servicios.</h3>
+      <div class="row mt-5">
+        <div class="col-sm-6 col-lg-4">
+          <div class="card bg-card-person rounded-lg mx-auto" style="width: 90%;">
+          <img src="https://randomuser.me/api/portraits/women/68.jpg" class="card-img-top rounded-circle img-card-person" alt="Testimonio">
+          <div class="card-body">
+            <p class="card-text text-center d-block" style="height:180px;">"Con mi tienda express pasé de tener todo el inventario guardado en un local por la cuarentena a moverlo de manera virtual, pensé que era muy complejo, ahora me compran desde ciudades donde jamas hubiera llegado"</p>
+            <h4 class="text-center" >Lili Valencia</h4>
+            <a class="text-center d-block" href="https://lilivalenciashoes.com" target="_blanck">www.lilivalenciashopes.com</a>
+            <div class="stars text-center">
+              <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+            </div>
+          </div>
+        </div>
+        </div>
+        <div class="col-sm-6 col-lg-4">
+          <div class="card bg-card-person rounded-lg mx-auto" style="width: 90%;">
+          <img src="https://randomuser.me/api/portraits/men/57.jpg" class="card-img-top rounded-circle img-card-person" alt="Testimonio">
+          <div class="card-body">
+            <p class="card-text text-center" style="height:180px;">"Puede personalizar mi tienda con algunos desarrollos como limitar la ubicación de mi servicio por ciudades, el proceso fue claroy logré mi tienda tal cual la necesitaba"</p>
+            <h4 class="text-center" >Felipe Gomez</h4>
+            <a class="text-center d-block" href="http://www.lujosexpress.com" target="_blanck">www.lujosexpress.com</a>
+            <div class="stars text-center">
+              <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+            </div>
+          </div>
+        </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 mx-auto">
+          <div class="card bg-card-person rounded-lg mx-auto" style="width: 90%;">
+          <img src="https://randomuser.me/api/portraits/men/13.jpg" class="card-img-top rounded-circle img-card-person" alt="Testimonio">
+          <div class="card-body">
+            <p class="card-text text-center" style="height:180px;">"La estrategia e-commerce del grupo Oster ha sido desarrollada sobre la plataformaV-tex, manejamos descuentos inventarios de todas nuestras tiendas y lanzamos promociones con aliados"</p>
+            <h4 class="text-center">Oster Colombia</h4>
+            <a class="text-center d-block" href="https://ostercolombia.com" target="_blanck">www.ostercolombia.com</a>
+            <div class="stars text-center">
+              <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+    <div class=" text-center">
+      <img class="imgms" src="@/assets/img/002_1.jpg" alt="CPC">
+      <h2 class="break display-1 font-weight-bold">Es momento <br>
+      <span>de ponernos en contacto</span></h2>
+    </div>
+    <!--section6-->
+    <div class="bkt2 mx-auto">
+      <div class="bkt2-blue mx-auto">
+        <div class="mx-auto" style="width:50%; padding-top:320px;">
+          <h2 class="text-center text-white font-weight-bold" style="font-size:2.5rem">Responde las siguientes preguntas <br> para saber que tipo de tienda deberías<br> montar deacuerdo a tu negocio</h2>
+         <button class="btn btn-test mx-auto btn-success rounded-pill btn-lg mb-5 d-block" data-toggle="modal" data-target="#Test">Hacer test</button>
+        </div>
+      </div>
+    </div>
+    <!--baner logos-->
+    
+      <div class="div baner-footer pt-1 mt-3">
+      <div class="logos logos-short mx-auto align-middle mt-3">
+          <a href="https://www.cpcagencia.com/#!/" target="_blanck">
+            <img class="logo mx-auto" src="@/assets/img/cpc.png" alt="CPC Agencia">
+          </a>
+          <a href="https://www.mc.net.co/" target="_blanck">
+            <img class="logo mx-auto" src="@/assets/img/Mc.png" alt="Media Commerce">
+          </a>
+        </div>
+    </div>
+    <footer>
+       <p class="text-center w-100 font-weight-bold">All Right Reserved to <strong>911harm</strong>&copy;2020</p>
+    </footer>
+    <!-- Modal Form -->
+<div class="modal fade" id="Test" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="tituloForm">¿Que tienda Virtual Necesitas?</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form class="form-group">
+          <div class="form-group">
+    <label>¿Tienes ya un hosting y/o dominio adquirido para tu nueva Tienda Virtual?</label>
+    <select v-model="p1" class="form-control" id="exampleFormControlSelect1">
+      <option :value="1">Si</option>
+      <option :value="1">No</option>
+      <option :value="1">No lo sé</option>
+    </select>
+  </div>
+   <div class="form-group">
+    <label>
+¿En cuánto tiempo necesitas que tu Tienda Virtual esté al aire?</label>
+    <select v-model="p2" class="form-control" id="exampleFormControlSelect1">
+      <option :value="1" >Puedo esperar 1 semana</option>
+      <option :value="3" >Puedo esperar 2 y 4 semanas</option>
+      <option :value="5" >Puedo esperar más de 4 semanas</option>
+    </select>
+  </div>
+   <div class="form-group">
+    <label>¿Cuántas tiendas físicas o puntos de atención/venta tienes actualmente?</label>
+    <select  v-model="p3" class="form-control" id="exampleFormControlSelect1">
+      <option :value="1">0</option>
+      <option :value="3">1</option>
+      <option :value="5">Más de 1</option>
+    </select>
+  </div>
+   <div class="form-group">
+    <label>
+¿Qué tan personalizable quieres tu Tienda Virtual?</label>
+    <select  v-model="p4" class="form-control" id="exampleFormControlSelect1">
+      <option :value="1">
+Básica: Que me permita actualizar Logo, banners, categorías, productos, referencias, precios, blog, direcciones, políticas de pago y botón de whatsapp.</option>
+      <option :value="3">
+Media: Que me permita actualizar Logo, colores, Categorias, productos, referencias, precios, Upselling, politicas de pago, tipos de clientes, tipos de usuarios, notificaciones por correo, vendedores, canales de ventas</option>
+      <option :value="5">
+Full: Que pueda actualizar siempre mis productos, personalizar espacios y crear desarrolos a medida como probadores de ropa, personalización de productos, etc. </option>
+    </select>
+  </div>
+   <div class="form-group">
+    <label>
+¿Tienes un equipo asignado internamente en tu empresa que te ayude a manejar tu Tienda Virtual?</label>
+    <select  v-model="p5" class="form-control" id="exampleFormControlSelect1">
+      <option :value="1">
+No, busco hacerlo yo mismo</option>
+      <option :value="3">
+Si, un equipo de mercadeo pequeño al que podría capacitar</option>
+      <option :value="5">
+Si, un equipo de mercadeo y TI robusto</option>
+    </select>
+  </div>
+   <div class="form-group">
+    <label>¿Cómo podrías cargar inventario de tus productos o servicios en la Tienda Virtual?</label>
+    <select  v-model="p6" class="form-control" id="exampleFormControlSelect1">
+      <option :value="1">
+Manualmente</option>
+      <option :value="4">
+Tengo un software de inventarios que podría integrar</option>
+      <option :value="4">
+Tengo un software de inventarios que podría integrar
+      </option>
+    </select>
+  </div>
+   <div class="form-group">
+    <label>¿Cómo se daría la facturación en tu Tienda Virtual?</label>
+    <select  v-model="p7" class="form-control" id="exampleFormControlSelect1">
+      <option :value="1">
+La hago manualmente</option>
+      <option :value="3">
+Quiero que la haga mi Tienda Virtual</option>
+      <option :value="5">
+Tengo un software que podría integrar con mi Tienda Virtual para facturar</option>
+    </select>
+  </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <!--<button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>-->
+        <button @click="sumaPuntaje" type="button" data-toggle="popover" data-target="#express" class="btn btn-test mx-auto">CONOCE LA TIENDA IDEAL PARA TI</button>
+      </div>
+    </div>
+  </div>
+</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App', data(){
+    return{
+      puntaje:0,
+      tienda:0,
+      p1:0,
+       p2:0,
+        p3:0,
+         p4:0,
+          p5:0,
+           p6:0,
+            p7:0,
+
+    }
+  },
+  methods:{
+    sumaPuntaje(){
+       this.puntaje=this.p1+this.p2+this.p3+this.p4+this.p5+this.p6+this.p7;
+        if(this.puntaje<=10){
+          this.tienda=0;
+
+        }
+        if(this.puntaje>10 && this.puntaje<=21){
+          this.tienda=1;
+          
+        }
+        if(this.puntaje>=22)
+        {this.tienda=2
+        }
+
+
+       
+    }
+  },
+  
+  components: {}
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style>
+*{
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+.nav{
+  width: 100%;
+  height:100px;
+  /*background: #004B96;*/
+  background-color: #3D57DF;
+  opacity: 0.8;
+  margin-top: 2px;
+  z-index: 1;
+}
+.logos{
+  width: 700px;
+  cursor: pointer;
+}
+.logo{
+  padding: 0 10px;
+  margin: 0;
+
+}
+.redes{
+  display: inline-block;
+  padding-left: 20px;
+  vertical-align:middle;
+}
+li{
+  font-size: 2rem;
+  list-style-type: none;
+  float: left;
+  padding: 10px;
+}
+li a{
+  text-decoration: none;
+  color:white;
+  cursor: pointer;
+}
+.bkt{
+  position: absolute;
+  top: 0px;
+  left: 15px;
+  width: 98%;
+  height: 605px;
+  background-image: url("assets/img/002.jpg");
+  background-position: center;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+}
+.btn-test{
+  background-color:#84E100;
+  color:#000631;
+  font-weight: bold;
+  font-size: 1.8rem;
+  width:300px;
+}
+.btn-more{
+      font-size: 1.2rem;
+    position: relative;
+    display: block;
+    bottom: 0;
+    width: 120px;
+    border-style: initial;
+}
+.title-md{
+  margin-top:200px;
+}
+.title-blue-dark{
+  color:#000631;
+}
+.section2{
+  
+  padding-top: 500px;
+  text-align: center;
+}
+.card{
+  border: none;
+}
+.img-card-tiendas{
+  width: 95px;
+  height: 130px;
+  padding-top: 30px;
+  float: left;
+}
+.card-body-tiendas{
+  height: 410px;
+}
+.title-section2{
+  color: #3D57DF;
+}
+.section4{
+  width: 100%;
+  background-color: #E5E6E8;
+  height: 900px;
+}
+.title-center{
+  text-align: center;
+}
+.fondo2{
+  width: 80%;
+  height: 90%;
+  background-image: url("assets/img/001.jpg");
+  background-position: center;
+  background-size: cover;
+}
+.img-card-person{
+  border-color:#3D57DF;
+  border-style:  solid;
+  border-width: 0.9rem;
+}
+.bg-card-person{
+  background-color: #E5E6E8;
+  height: 630px;
+
+}
+.stars{
+  font-size: 1.5rem;
+  color:#D645BD;
+  
+}
+.stars i {
+  padding-right: 4px;
+}
+.imgms{
+  transform: rotate(-180deg);
+  height: 200px;
+  width: 100%;
+}
+.break{
+  position: relative;
+  top:-150px;
+  color:white;
+  font-size: 5rem;
+  line-height: 0.4;
+}
+.break span{
+  font-size: 2rem;
+}
+.bkt2{
+  background-image: url("assets/img/shutterstock_610931219.jpg");
+  background-position: center;
+  background-size: cover;
+  height: 900px;
+  width: 98%;
+}
+.bkt2-blue{
+ background-color:rgba(0, 75, 150, 0.8);
+ height: 100%;
+}
+.baner-footer{
+ /*background-color:rgba(0, 75, 150, 0.8);*/
+ background-color: #3D57DF;
+  width:100%;
+  height:100px;
+}
+.logos-short{
+  width: 480px;
 }
 </style>
